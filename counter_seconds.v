f1a6c17694b25476c2b3d34d27e9e6551d20f578
dark-knight16 <ledangquang16022005@gmail.com>
2025-08-18 07:24:50 +0700

    Add design & testbench of counter_seconds

---
+++ counter_seconds.v
@@ -0,0 +1,63 @@
+module counter_seconds (
+  input clk,
+  input rst_n,
+  input mode_second,
+  input up, 
+  input down, 
+  output reg [3:0] second_unit,
+  output reg [3:0] second_ten,
+  output reg tick_second
+);
+
+  always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+      second_unit <= 4'd0;
+      second_ten <= 4'd0;
+      tick_second <= 0;
+    end  
+    else begin
+      if (mode_second) begin
+        if (second_unit == 4'd9) begin
+          second_unit <= 4'b0000;
+          if (second_ten == 4'd5) begin
+            second_ten <= 4'd0;
+            tick_second <= 1'b1;
+          end
+          else begin
+            second_ten <= second_ten + 1'b1;
+            tick_second <= 0;
+          end
+        end
+        else begin
+          second_unit <= second_unit + 1'b1;
+          tick_second <= 1'b0;
+        end
+      end
+      else begin
+        case ({up, down})
+        2'b10: begin
+          if (second_unit == 4'd9) begin
+            second_unit <= 4'b0000;
+            if (second_ten == 4'd5) second_ten <= 4'd0;
+            else second_ten <= second_ten + 1'd1;
+          end
+          else second_unit <= second_unit + 1'd1;
+        end
+        2'b01: begin
+          if (second_unit == 4'd0) begin
+            second_unit <= 4'd9;
+            if (second_ten == 4'd0) second_ten <= 4'd5;
+            else second_ten <= second_ten - 1'd1;
+          end
+          else second_unit <= second_unit - 1'd1;
+        end
+        default: begin
+          second_unit <= second_unit;
+          second_ten <= second_ten;
+        end
+      endcase
+      end
+    end
+  end
+
+endmodule
---
+++ counter_seconds_tb.v
@@ -0,0 +1,59 @@
+`timescale 1ns/1ps
+
+module counter_seconds_tb;
+
+  reg clk;
+  reg rst_n;
+  reg mode_second;
+  reg up;
+  reg down;
+  wire [3:0] second_unit;
+  wire [3:0] second_ten;
+  wire tick_second;
+
+  counter_seconds cnt_sec (
+    .clk(clk),
+    .rst_n(rst_n),
+    .mode_second(mode_second),
+    .up(up),
+    .down(down),
+    .second_unit(second_unit),
+    .second_ten(second_ten),
+    .tick_second(tick_second)
+  );
+
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+  end
+
+  initial begin 
+    rst_n = 0;
+    mode_second = 1;
+    up = 0;
+    down = 0;
+
+    #20 rst_n = 1;
+
+    mode_second = 1;
+    repeat(65) @(posedge clk);
+
+    mode_second = 0;
+
+    up = 1; down = 1;
+    repeat(65) @(posedge clk);  
+
+    up = 1; down = 0;
+    repeat(65) @(posedge clk);  
+   
+    up = 0; down = 0;
+    repeat(65) @(posedge clk);
+
+    up = 0; down = 1;
+    repeat(65) @(posedge clk);
+
+    #2000 $stop;
+
+  end
+
+endmodule
